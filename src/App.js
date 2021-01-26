bc1b37b748be411f8ec62affce5a5b8e3f8a1104
Zoriana-Burian <64468262+Zoriana-Burian@users.noreply.github.com>
2021-01-26 21:08:19 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Searchbar from './components/Searchbar/Searchbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    hits: [],
+    currentPage: 1,
+  };
+ 
+
+  onChangeQuery = query => {
+   // console.log(query)
+    axios.get(`https://pixabay.com/api/?key=19054407-f71c417dea239df63beb23abd&q=${query}&image_type=photo&orientation=horizontal&per_page=12`)
+      .then(response => {
+      //  console.log(response.data.hits);
+        this.setState({
+          hits: response.data.hits,
+        })
+      });
+    }
+  
+
+  render() {
+    const { hits } = this.state;
+    return <div>
+      <Searchbar onSubmit={this.onChangeQuery}/>
+      <ul className="ImageGallery">
+        {hits.map(({ id, webformatURL, largeImageURL }) => <li key={id}>
+         <a href={webformatURL}><img src={largeImageURL} alt=''></img></a>
+  </li>)}
+</ul>
+
+      <button type='button'>Load more</button>
+    </div>;
+  }
 }
 
 export default App;
