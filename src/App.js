8fb353cef5114202a7f45414a415d2c0ff3d37ee
Zoriana-Burian <64468262+Zoriana-Burian@users.noreply.github.com>
2021-01-28 15:55:17 +0200

    d

--- src/App.js
+++ src/App.js
@@ -9,32 +9,52 @@ class App extends Component {
   state = {
     hits: [],
     currentPage: 1,
+    searchQuery: '',
+    isLoading: false,
   };
  
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.fetchHits();
+    }
+  }
 
   onChangeQuery = query => {
-   // console.log(query)
-    axios.get(`https://pixabay.com/api/?key=19054407-f71c417dea239df63beb23abd&q=${query}&image_type=photo&orientation=horizontal&per_page=12`)
+    this.setState({ searchQuery: query, currentPage: 1, hits: [] });
+    
+  }
+  
+  fetchHits = () => {
+    const { currentPage, searchQuery } = this.state;
+    this.setState({ isLoading: true });
+    axios.get(`https://pixabay.com/api/?key=19054407-f71c417dea239df63beb23abd&q=${searchQuery}&page=${currentPage}&image_type=photo&orientation=horizontal&per_page=12`)
       .then(response => {
       //  console.log(response.data.hits);
-        this.setState({
-          hits: response.data.hits,
-        })
-      });
-    }
+        this.setState(prevState => ({
+          hits: [...prevState.hits, ...response.data.hits],
+          currentPage: prevState.currentPage + 1,
+        }))
+      }).finally(() => this.setState({isLoading: false}));
+  }
   
 
   render() {
-    const { hits } = this.state;
+    const { hits, isLoading } = this.state;
     return <div>
       <Searchbar onSubmit={this.onChangeQuery}/>
+      
       <ul className="ImageGallery">
         {hits.map(({ id, webformatURL, largeImageURL }) => <li key={id}>
          <a href={webformatURL}><img src={largeImageURL} alt=''></img></a>
   </li>)}
-</ul>
+      </ul>
 
-      <button type='button'>Load more</button>
+      {isLoading && <h1>Завантаження...</h1>}
+      
+      {hits.length > 0 && !isLoading &&
+        <button type='button' onClick={this.fetchHits}>Load more</button>
+    }
+      
     </div>;
   }
 }
